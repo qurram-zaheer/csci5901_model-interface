6e5ec4a565806c4ac7b2b901ea6f7b1ff2b0c8e3
Qurram Zaheer Syed <qr620423@dal.ca>
2022-08-10 14:06:04 -0300

    updating endpoint

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ st.title("Qurram's CSCI5901 project dashboard")
 
 tab1, tab2 = st.tabs(["Interface", "Monitoring"])
 
+BASE_URL = "http://ec2-44-201-255-228.compute-1.amazonaws.com:8501/v1/models/model"
+
 with tab1:
     st.header("Model interface")
 
-    status_dict = list(requests.get("http://ec2-3-89-36-216.compute-1.amazonaws.com:8501/v1/models/model").json().values())[0][0]
+    status_dict = list(requests.get(f'{BASE_URL}').json().values())[0][0]
     if status_dict['state'] == 'AVAILABLE':
         st.markdown(f'<p style="color:white;font-size:24px;border-radius:2%;width:120px;border:1px solid '
                     f'#09ab3b;text-align:center;background-color:#09ab3b;border-radius:5px">AVAILABLE</p>',
@@ -30,7 +32,7 @@ with tab1:
         data = json.dumps({"signature_name": "serving_default", "instances": random_entry.values.reshape(1, -1).tolist()})
 
         headers = {"content-type": "application/json"}
-        json_response = requests.post('http://ec2-3-89-36-216.compute-1.amazonaws.com:8501/v1/models/model:predict', data=data, headers=headers)
+        json_response = requests.post(f'{BASE_URL}:predict', data=data, headers=headers)
         print("Response", json_response.json())
         pred = json_response.json()["predictions"][0][0]
         st.markdown(f'<span style="color:#a3a8b4;">Probability of hospital death: </span> <span style=color:{"#ff2b2b" if pred > 0.5 else "#09ab3b"}>{pred}</span>', unsafe_allow_html=True)
